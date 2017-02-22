4b28585e819f75d5ac023f9a5c7b5a4bdb0b27fd
charels-lei <charles.lei@activenetwork.com>
2017-02-22 08:28:57 +0800

    js update

--- js/jcalendar.js
+++ js/jcalendar.js
@@ -32,6 +32,11 @@ function JCalendar() {
 	JCalendar.cur_year = current_year;
 	JCalendar.cur_month = current_month;
 	JCalendar.cur_date = current_day;
+	
+		//成员变量，当前年月日
+	JCalendar.selected_year = current_year;
+	JCalendar.selected_month = current_month;
+	JCalendar.selected_date = current_day;
 }
 JCalendar.prototype.show = function() {
 	//日历里的每个单元格的数据，预先定义一段空数组，对应日历里第一周空的位置。[注意星期天对应的数是0]
@@ -78,11 +83,11 @@ function calendar_init() {
 	JCalendar.getRecentDay = function() {
 		var recent_day;
 		for(var i = 0; i < json.monthEvents.length; i++) {
-			if(json.monthEvents[i].month == JCalendar.cur_month) {
+			if(json.monthEvents[i].month == JCalendar.cur_month && json.monthEvents[i].year == JCalendar.cur_year) {
 				recent_day = json.monthEvents[i].events[0].date;
 				for(var j = 0; j < json.monthEvents[i].events.length; j++) {
-					if(json.monthEvents[i].events[j].date >= JCalendar.current_day) {
-						recent_day = json.monthEvents[i].event[j].date;
+					if(json.monthEvents[i].events[j].date >= JCalendar.cur_date) {
+						recent_day = json.monthEvents[i].events[j].date;
 						break;
 					}
 				}
@@ -106,6 +111,7 @@ function calendar_init() {
 					}
 					if(recentDay == th) {
 						jQuery(this).addClass("current");
+						JCalendar.selected_date = recentDay;
 					}
 				});
 			}
@@ -117,6 +123,7 @@ function calendar_init() {
 		var fday = date.getDay(); //每月第一天的星期数
 		var year = date.getFullYear();
 		var month = date.getMonth() + 1;
+			
 		var dayNum = new Date(JCalendar.cur_year, parseInt(JCalendar.cur_month) + _month, 0).getDate(); //每月的天数
 		var tds = document.getElementById("calendar").getElementsByTagName("td");
 		for(var i = 7; i < tds.length; i++) //清空日历内容
@@ -136,7 +143,7 @@ function calendar_init() {
 
 		//获取日期数据状态
 		for(var i = 0; i < json.monthEvents.length; i++) {
-			if(JCalendar.cur_month == json.monthEvents[i].month) {
+			if(JCalendar.cur_month == json.monthEvents[i].month && JCalendar.cur_year == json.monthEvents[i].year) {
 				for(var k = 0; k < json.monthEvents[i].events.length; k++) {
 					jQuery("#calendar tr td div").each(function() {
 						//console.log()
@@ -146,17 +153,27 @@ function calendar_init() {
 							jQuery(this).addClass("has");
 							jQuery(this).attr('onmouseover', 'JCalendar.click(this)');
 						}
+						if(JCalendar.selected_year == JCalendar.cur_year && JCalendar.cur_month == JCalendar.selected_month&& th == parseInt(JCalendar.selected_date)){
+							jQuery(this).addClass("current");
+						}
 					});
-					if(json.monthEvents[i].events[k].date == JCalendar.cur_date) {
-						jQuery("#calendar tr td div").each(function() {
-							if(jQuery(this).html() == parseInt(JCalendar.cur_date)) {
-								jQuery(this).addClass("current");
-							}
-						});
-					}
 				}
 			}
 		}
+		if(parseInt(_month) >0  && (current_year != JCalendar.cur_year || current_month != JCalendar.cur_month)){
+			jQuery(".next").css("visibility","hidden")
+		}
+		else{
+			jQuery(".next").css("visibility","visible")
+		}
+		
+		if(parseInt(_month) <0  && (current_year != JCalendar.cur_year || current_month != JCalendar.cur_month)){
+			jQuery(".prev").css("visibility","hidden")
+		}
+		else{
+			jQuery(".prev").css("visibility","visible")
+		}
+		
 		JCalendar.onupdate(year, month, JCalendar.cur_date);
 	}
 	JCalendar.onupdate = function(year, month, date) { //日历更改时执行的函数，可以更改为自己需要函数,控件传递过来的参数为当前日期
@@ -202,6 +219,10 @@ function calendar_init() {
 		JCalendar.cur_year = year;
 		JCalendar.cur_month = month;
 		JCalendar.cur_date = date;
+		
+		JCalendar.selected_year = year;
+		JCalendar.selected_month = month;
+		JCalendar.selected_date = date;
 		fit_height();
 		
 		var eventCount = 0;
@@ -214,8 +235,7 @@ function calendar_init() {
 			jQuery(".carousel-control").css("visibility","visible")
 		}
 		else{
-			jQuery(".carousel-control").css("visibility","hidden
-			")
+			jQuery(".carousel-control").css("visibility","hidden")
 		}
 	}
 	
